a383881746fb2728e23275cc4cb0f853cd323c80
jDev <dev@temposys.name>
2018-08-23 12:57:14 +0300

    #7126 Змінити картинку done

--- resources/views/animals/_no_animals.blade.php
+++ resources/views/animals/_no_animals.blade.php
@@ -1,5 +1,5 @@
 <div class="no-pets-container">
-    <img src="{{ asset('/img/no_pets.jpg') }}" alt="Немає домашніх тварин">
+    <img src="{{ asset('/img/no_pets.png') }}" alt="Немає домашніх тварин">
     <div class="title">Додайте свого домашнього улюбленця</div>
     <span class="gray">На жаль, ви ще не додали жодного улюбленця :(</span>
 </div>
