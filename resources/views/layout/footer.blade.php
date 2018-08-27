790309e522f95d5a14685dc4c7040b883886658c
jDev <dev@temposys.name>
2018-08-27 14:37:12 +0300

    #7128 Змінити футер done

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="footer-demo">
+    <div class="footer-demo text-white text-left">
         Сайт працює в тестовому режимі. Якщо ви маєте зауваження або пропозиції, будь-ласка, напишіть нам:
         <a href="mailto:support.rdt@kyivcity.gov.ua ">support.rdt@kyivcity.gov.ua </a> або зателефонуйте за номером:
         <a href="tel:0443668019">(044) 366-80-19</a>
@@ -20,7 +20,7 @@
         </div>
     </div>
     <div class="footer-kiev-portal">
-        <div class="col-about">
+        <div class="col col-about">
             <div class="col-about-header">
                 <img src="{{ asset('img/icon/gerb.png') }}" alt="Герб" class="logo">
                 <div class="about-header-content">
@@ -29,37 +29,33 @@
                     <span class="sub-text">Київська міська державна адміністрація<br>Київська міська рада</span>
                 </div>
             </div>
-            <span class="col-about-addr">вул. Хрещатик, 36, м. Київ, 01044</span>
         </div>
-        <div class="col-contacts">
+        <div class="col col-contacts">
             <div class="contacts-item">
-                <span class="above-text">зі стаціонарного та мобільного</span>
-                <span>1551</span>
+                <span class="above-text">Служба підтримки користувачів</span>
+                <span class="above-text">"Реєстру домашніх тварин"</span>
+
             </div>
             <div class="contacts-item">
-                <span class="above-text">електронною поштою</span>
-                <span><a href="mailto:zvernen@kmda.gov.ua" class="text-white">zvernen@kmda.gov.ua</a></span>
+                <span class="above-text">телефон: <span class="text-white">(044) 366-80-19</span></span>
+                <span class="above-text">e-mail: <a class="text-white" href="mailto:support.rdt@kyivcity.gov.ua ">support.rdt@kyivcity.gov.ua </a></span>
             </div>
         </div>
-        <div class="col-links">
-            <a href="#" class="col-links-item">Громадськості</a>
-            <a href="#" class="col-links-item">Публічна інформація та ЗМІ</a>
-            <a href="#" class="col-links-item">Київ та міська влада</a>
-            <a href="#" class="col-links-item">Контакти</a>
+        <div class="col col-about contacts-item">
+            <span class="above-text">Комунальне підприємство</span>
+            <span class="above-text">"Київська міська лікарня</span>
+            <span class="above-text">ветеринарноі медицини" (КП КМЛВМ)</span><br><br>
+            <span class="above-text">Електротехнічна 5-А місто Київ</span>
+            <span class="above-text">телефон: <span class="text-white">+38 (044) 366-6401</span></span>
+            <span class="above-text">e-mail: <a class="text-white" href="mailto:info@kmlvm.com.ua ">info@kmlvm.com.ua </a></span>
+            <span class="above-text">сайт: <a class="text-white" href="http://www.kmlvm.com.ua">http://www.kmlvm.com.ua </a></span>
+
         </div>
-    </div>
-    <div class="work-time">
-        <span>пн-чт з 9:00 до 18:00, пт з 9:00 до 16:45<br>Перерва з 13:00 до 13:45</span>
     </div>
     <div class="copyright">
         <div class="text">
             <span>&copy; 2018 Київська міська державна адміністрація, Київська міська рада. Реалізовано в рамках проекту</span>
             <a href="#">Kyiv Smart City</a>
-        </div>
-        <div class="links">
-            <a href="#">Умови використання</a>
-            <a href="#">Політика конфіденційності</a>
-            <a href="#">Карта порталу</a>
         </div>
     </div>
 </footer>
