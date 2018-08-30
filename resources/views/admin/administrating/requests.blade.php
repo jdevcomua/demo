0d64088d93346d796649263a172349314083e198
Adam M <adammospan@gmail.com>
2018-08-30 16:54:57 +0300

    changed default sorting on requests page

--- resources/views/admin/administrating/requests.blade.php
+++ resources/views/admin/administrating/requests.blade.php
@@ -80,6 +80,7 @@
 
             dataTableInit($('#datatable'), {
                 ajax: '{{ route('admin.administrating.requests.data') }}',
+                order: [[ 11, "desc" ]], // def. sort by created_at DESC
                 columns: [
                     { "data": "id" },
                     { "data": "user" ,
