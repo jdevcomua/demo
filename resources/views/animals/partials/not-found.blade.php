bc46ee8f5acd8a4af7147eef9d15dabe91d937e4
Adam M <adammospan@gmail.com>
2018-08-30 11:53:59 +0300

    changed text on notfound modal

--- resources/views/animals/partials/not-found.blade.php
+++ resources/views/animals/partials/not-found.blade.php
@@ -6,7 +6,7 @@ function () {
         '<div class="d-flex flex-column justify-content-center text-center">'+
             '<img src="" alt="">'+
             '<h2>На жаль, тварина не знайдена</h2>'+
-            '<p class="not-found">Надішліть запит для пошуку вашої тварини<br>'+
+            '<p class="not-found">Щоб знайти вашу тварину в реєстрі, будь ласка, надішліть запит адміністраторам системи<br>'+
             '<span>або</span> <br>'+
             'зареєструйте улюбленця ще раз</p>'+
             '<button type="button" class="btn not-found-search mt-6 btn-primary">Запит на пошук</button>' +
