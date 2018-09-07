1ee51266a69594e59bc42e13a2d8be9117617069
Adam M <adammospan@gmail.com>
2018-09-07 13:05:52 +0300

    refusing user login with no phones

--- app/Auth/KyivIdUserResolver.php
+++ app/Auth/KyivIdUserResolver.php
@@ -18,6 +18,7 @@ class KyivIdUserResolver
     {
         if (!$user) return null;
         if (!$user->inn && !$user->passport) return null;
+        if (!$user->phones) return null;
 
         //Searching by external kievID
         $existUser = User::where('ext_id', '=', $user->ext_id)->first();
