68391ae51dcbd295bca8b6935c39081e7bdb5f20
Adam M <adammospan@gmail.com>
2019-01-28 09:32:53 +0200

    fix undefined user exception on show_contacts_owner view

--- resources/views/animals/show_contacts_owner.blade.php
+++ resources/views/animals/show_contacts_owner.blade.php
@@ -4,26 +4,33 @@
 
 @section('content')
     <div class="cols-block pb-0">
-        <div class="cols-block-header">
-            <div class="block-title">Контактні дані власника тварини</div>
-            <div class="block-sub-title"></div>
-        </div>
-        <div class="cols-block-content form">
-            <div class="form-group">
-                <label for="last_name">Ім'я</label>
-                <input type="text" class="form-control" id="last_name"
-                       value="{{$animal->user->first_name}}" readonly>
+        @if($animal->user)
+            <div class="cols-block-header">
+                <div class="block-title">Контактні дані власника тварини</div>
+                <div class="block-sub-title"></div>
             </div>
-            <div class="form-group">
-                <label for="last_name">Номер телефону</label>
-                <input type="text" class="form-control" id="last_name"
-                       value="{{$animal->user->phones[0]->phone}}" readonly>
+            <div class="cols-block-content form">
+                <div class="form-group">
+                    <label for="last_name">Ім'я</label>
+                    <input type="text" class="form-control" id="last_name"
+                           value="{{$animal->user->first_name}}" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Номер телефону</label>
+                    <input type="text" class="form-control" id="last_name"
+                           value="{{$animal->user->phones[0]->phone}}" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Пошта</label>
+                    <input type="text" class="form-control" id="last_name"
+                           value="{{$animal->user->primary_email->email}}" readonly>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="last_name">Пошта</label>
-                <input type="text" class="form-control" id="last_name"
-                       value="{{$animal->user->primary_email->email}}" readonly>
+        @else
+            <div class="cols-block-header">
+                <div class="block-title">Не знайдено</div>
+                <div class="block-sub-title">Власник не зареєстрований в системі</div>
             </div>
-        </div>
+        @endif
     </div>
 @endsection
