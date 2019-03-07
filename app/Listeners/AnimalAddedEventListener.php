c7f6f091a014d192654f78f61ee364f08162fbac
Serhii Bakumenko <serhii.bakumenko@gmail.com>
2019-03-07 02:13:55 +0200

    PETS-69 - send email after animal added

--- app/Listeners/AnimalAddedEventListener.php
+++ app/Listeners/AnimalAddedEventListener.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Events\AnimalAdded;
 use App\External\AnimalAddedOrder;
 use App\External\OrderService;
+use App\Mail\CustomMail;
+use Carbon\Carbon;
 
 class AnimalAddedEventListener
 {
@@ -26,6 +28,28 @@ class AnimalAddedEventListener
      */
     public function handle($event)
     {
+        $html = "
+<h4>Вітаємо!</h4>
+<p>Ви додали вашого улюбленця на сайті \"Реєстр домашніх тварин\".</p>
+<p>Для закінчення реєстрації тварини Вам необхідно звернутися до Київської міської лікарні ветеринарної медицини, за адресою:</p>
+<b>місто Київ</b><br><b>Електротехнічна, 5А</b><br><b>тел. +38 (044) 366 69 99</b><p></p>
+<p><br></p>
+<p>Час роботи:</p>
+<p>Пн-Чт 9:00 - 18:00 год</p>
+<p>Пт 9:00 - 16:45 год</p>
+<p>Обід 13:00 -14:00 год</p>
+<br><p><b>При собі мати:</b></p>
+- документ, що посвідчує особу <br>- ветеринарний паспорт <br>- тварину.&nbsp;<p></p>
+<p><br></p>
+<p>Більш детальну інформацію про реєстрацію тварин можна переглянути на сайті КП \"КМЛВМ\" https://bit.ly/2ISR5jH. </p>
+<br><p>З повагою, команда Kyiv Smart City</p>
+";
+        $message = (new CustomMail('Вітаємо!', $html))
+            ->onQueue('default')
+            ->delay(Carbon::now()->addSecond());
+
+        \Mail::to(\Auth::user()->primary_email)
+            ->queue($message);
         $order = new AnimalAddedOrder($event->user, $event->animal);
         $orderService = new OrderService($order);
         $orderService->sendData();
